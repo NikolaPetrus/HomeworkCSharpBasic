57417149e882cf0e2bc1c03b21a600ce735598d9
Nikola Petrusevski <petrusevskin01@outlook.com>
2021-04-20 17:53:17 +0200

    .

---
+++ Homework_03/LoopsAndArrays/StudentGroup/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] studentsG1 = { "Jana", "Ivan", "Stefan", "Matej", "Darko" };
+            string[] studentsG2 = { "Marko", "Nikola", "Martina", "Marija", "Andrej" };
+
+            Console.Write("Enter student group: (for group G1 enter 1 and for group G2 enter 2): ");
+            string input = Console.ReadLine();
+            while (true)
+            {
+                if (input == "1")
+                {
+                    foreach (var student in studentsG1)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                }
+                else if (input == "2")
+                {
+                    foreach (var student in studentsG2)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.Write("Invalid input! Try again: ");
+                    input = Console.ReadLine();
+                }
+
+            }
+        }
+
+    }
+}
+
---
+++ Homework_03/LoopsAndArrays/Task-1-SumofEven/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_1_SumofEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter six numbers:");
+            int checkFirstNumber;
+            int checkSecondNumber;
+            int checkThirdNumber;
+            int checkFourthNumber;
+            int checkFifthNumber;
+            int checkSixtNumber;
+
+            bool firstInput = int.TryParse(Console.ReadLine(), out checkFirstNumber);
+            bool secondInput = int.TryParse(Console.ReadLine(), out checkSecondNumber);
+            bool thirdInput = int.TryParse(Console.ReadLine(), out checkThirdNumber);
+            bool fourthInput = int.TryParse(Console.ReadLine(), out checkFourthNumber);
+            bool fifthInput = int.TryParse(Console.ReadLine(), out checkFifthNumber);
+            bool sixthInput = int.TryParse(Console.ReadLine(), out checkSixtNumber);
+
+
+            if (firstInput && secondInput && thirdInput && fourthInput && fifthInput && sixthInput)
+            {
+                int[] arrayOfNums = { checkFirstNumber, checkSecondNumber, checkThirdNumber, checkFourthNumber, checkFifthNumber, checkSixtNumber };
+                int sum = 0;
+                foreach (var num in arrayOfNums)
+                {
+                    if (num % 2 == 0)
+                    {
+                        sum += num;
+                    }
+
+                }
+                Console.WriteLine("The result is " + sum);
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter six numbers");
+            }
+        }
+    }
+}
