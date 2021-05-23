4e3fb6b4f207646006c14b64aedda43b271cebcc
Nikola Petrusevski <petrusevskin01@outlook.com>
2021-05-23 18:30:35 +0200

    added 5/6

---
+++ Homework_05/Homework_05/ClassesAndObjects/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjects
+{
+    public class Car
+    {
+            public string Model { get; set; }
+            public int Speed { get; set; }
+            public Driver Driver { get; set; }
+
+            public Car(string model, int speed)
+            {
+                Model = model;
+                Speed = speed;
+            }
+
+            public int CalculateSpeed()
+            {
+                int result = Driver.Skill * Speed;
+                return result;
+            }
+
+        }
+    }
